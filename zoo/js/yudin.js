01d411fd77ddfcd76c8f593642074a2c243668a1
code001_517 <934946337@qq.com>
2018-01-25 14:15:39 +0800

    zoo

---
+++ zoo/js/yudin.js
@@ -0,0 +1,8 @@
+$( function () {
+	$('.yudin').click( function () {
+		$('#yd_cover').css('display','block');
+	} );
+	$('.yd_close').click( function () {
+		$('#yd_cover').css('display','none');
+	} );
+} )
